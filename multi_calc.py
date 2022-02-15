ddc456a891d71ddb4854edebfc018552f9465d3f
Aleksandr Sazhin <sazhinalex.portal@gmail.com>
2022-02-15 20:55:26 +0500

    Some commit

--- multi_calc.py
+++ multi_calc.py
@@ -3,28 +3,99 @@ from tkinter import ttk
 
 
 class Figure(Frame):
-    def __init__(self, root):
+    def __init__(self, root, name: str):
         super().__init__(root)
-        self._canvas = Canvas(self, width=300, height=300, bg='white')
+        self._name = name
+        self._canvas = Canvas(self, width=500, height=500, bg='white')
         self._canvas.pack()
 
     def area(self):
         raise NotImplementedError('Нереализованный метод!')
 
     def get_name(self):
+        return self._name
+
+    def draw(self):
         raise NotImplementedError('Нереализованный метод!')
 
 
 class Rectangle(Figure):
     def __init__(self, root, name: str):
-        super().__init__(root)
-        self.__name = name
+        super().__init__(root, name=name)
+        self._combo = ttk.Combobox(self, values=Rectangle.what_calc(),
+                                   state='readonly')
+        self._combo.pack(side=LEFT, padx=5, pady=10)
+        self._combo.current(0)
+
+    @staticmethod
+    def what_calc() -> tuple:
+        return 'Площадь', 'Периметр'
 
     def area(self):
         pass
 
-    def get_name(self) -> str:
-        return self.__name
+    def draw(self):
+        pass
+
+
+class Square(Figure):
+    def __init__(self, root, name: str):
+        super().__init__(root, name=name)
+
+    @staticmethod
+    def what_calc() -> tuple:
+        return 'Площадь', 'Периметр'
+
+    def area(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Triangle(Figure):
+    def __init__(self, root, name: str):
+        super().__init__(root, name=name)
+
+    @staticmethod
+    def what_calc() -> tuple:
+        return 'Площадь', 'Периметр'
+
+    def area(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Circle(Figure):
+    def __init__(self, root, name: str):
+        super().__init__(root, name=name)
+
+    @staticmethod
+    def what_calc() -> tuple:
+        return 'Площадь', 'Периметр'
+
+    def area(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Cube(Figure):
+    def __init__(self, root, name: str):
+        super().__init__(root, name=name)
+
+    @staticmethod
+    def what_calc() -> tuple:
+        return 'Площадь', 'Периметр', 'Объем'
+
+    def area(self):
+        pass
+
+    def draw(self):
+        pass
 
 
 # Калькулятор может быть только один. На данный момент...
@@ -42,7 +113,7 @@ class Calculator(metaclass=MetaCalculator):
 
     def __init__(self):
         self.__root = Tk()
-        self.__root.geometry('400x400')
+        self.__root.geometry('500x700')
         self.__root.resizable(False, False)
         self.__notebook = ttk.Notebook(self.__root)
         self.__notebook.pack()
